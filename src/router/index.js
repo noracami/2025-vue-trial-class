40e6a70d9a2ec92978edf4f4a41977fc90905aa1
noracami <kerke.2011@gmail.com>
2025-08-12 03:16:48 +0800

    [chore] format code

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -15,6 +15,6 @@ const router = createRouter({
       component: () => import('../views/Week1View.vue'),
     },
   ],
-})
+});
 
-export default router
+export default router;
